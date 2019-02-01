0d061f3e5c93f80595376f5a19dca6f8cd208bcf
Joshua Crewe <joshua@venncreative.co.uk>
2019-02-01 21:10:57 +0000

    Add support for simple shell commands

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -2,6 +2,8 @@ import {Command} from '@oclif/command'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
+import shell from '../../lib/shell'
+
 export default class New extends Command {
     static description = 'Scaffold a project from a git repo'
 
@@ -10,11 +12,15 @@ export default class New extends Command {
     }]
 
     async run() {
+        shell('git', ['-v'], () => {
+            this.log('run it')
+        })
         const {args} = this.parse(New)
         const userConfig = await fs.readJSON(path.join(this.config.configDir, 'config.json'))
         if (userConfig.projects[0][args.project]) {
             this.log(userConfig.projects[0][args.project])
             // @TODO: Set up the prject using this name and URL
+
         } else {
             this.log('no project by that name')
 
